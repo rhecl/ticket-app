284a9af6a8b8cd0ece159078f49fc308c0c745d4
rhecl <rok.hecl89@gmail.com>
2020-09-15 23:53:09 +0200

    update

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
@@ -32,6 +33,15 @@ router.post('/api/users/signup', [
 
   const user = User.build({ email, password });
   await user.save();
+
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email
+  }, 'asdf'); // temp secret
+
+  req.session = {
+    jwt: userJwt
+  };
 
   res.status(201).send(user);
 });
