8f2580b5cd7344763447b6fd5d1acb4763726d44
rhecl <rok.hecl89@gmail.com>
2020-09-17 21:30:57 +0200

    update

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,38 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
+
+// Execute during server side rendering with nextjs
+// Whatever is returned is sent to the component as props
+LandingPage.getInitialProps = async ({ req }) => {
+  // request from getInitialProps can be executed on the server OR client
+  // depending on how the page was loaded
+  // refresh, URL reload => on the server
+  // navigating inside the app => on the client
+
+  // on the server
+  if (typeof window === 'undefined') {
+    // access ingress nginx from inside the cluster
+    // SERVICE-NAME.NAMESPACE.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        // headers: {
+        //  host: 'tickets.dev',
+        //},
+        headers: req.headers, // include all initial headers, including the jwt cookie
+      }
+    );
+
+    return data;
+  }
+
+  const { data } = await axios.get('/api/users/currentuser');
+
+  return data;
+};
+
+export default LandingPage;
